b5d76dbe6a708dc5b21e3bc2dce25c6dd9761019
Ziad Alaa <ziadalaa955@gmail.com>
2026-02-11 20:48:44 +0200

    minor updates OOP

--- OOP/Day 2/Assignment2.3/Assignment2.3/Assignment2.3.cpp
+++ OOP/Day 2/Assignment2.3/Assignment2.3/Assignment2.3.cpp
@@ -8,10 +8,12 @@ int main()
 	bool flag;
 
 	cout << "Enter number of rows and columns (ODD): " << endl;
-	do {
+	do
+	{
 		flag = false;
 		cin >> n;
-		if (n % 2 == 0 || n <= 0) {
+		if (n % 2 == 0 || n <= 0)
+		{
 			cout << "Enter only odd numebr!" << endl;
 			flag = true;
 		}
@@ -22,20 +24,23 @@ int main()
 	int color = 31;
 
 	int row = 0, col = n / 2;
-	for (int i = 1; i <= n * n; i++) {
-		if (color == 37)color = 31;
-		cout << "\033["<<color<<"m";
-		cout << "\033[" << row + 1 << ";" << (col * 4 + 1) <<"H" << i;
+	for (int i = 1; i <= n * n; i++)
+	{
+		if (color == 37)
+			color = 31;
+		cout << "\033[" << color << "m";
+		cout << "\033[" << row + 1 << ";" << (col * 4 + 1) << "H" << i;
 		color++;
 
-		int newrow = (row - 1 + n) % n;	 //move one row up
-		int newcol = (col + 1) % n; //move column right
-		
+		int newrow = (row - 1 + n) % n; // move one row up
+		int newcol = (col + 1) % n;		// move column right
+
 		if ((i % n) == 0) // when the next cell is filled (i%n is multiple of n)
 		{
 			row = (row + 1) % n; // go down one cell
 		}
-		else {
+		else
+		{
 			row = newrow;
 			col = newcol;
 		}
--- OOP/Day 5/Assignments/Assignemnet 5.1/Assignemnet 5.1/Assignemnet 5.1.cpp
+++ OOP/Day 5/Assignments/Assignemnet 5.1/Assignemnet 5.1/Assignemnet 5.1.cpp
@@ -2,14 +2,16 @@
 
 using namespace std;
 
-void swapByAddress(int* ptrx, int* ptry) {
+void swapByAddress(int *ptrx, int *ptry)
+{
 	int temp;
 	temp = *ptrx;
 	*ptrx = *ptry;
 	*ptry = temp;
 }
 
-void swapByRefrence(int& x, int& y) {
+void swapByRefrence(int &x, int &y)
+{
 	int temp;
 	temp = x;
 	x = y;
@@ -20,9 +22,9 @@ int main()
 {
 	int x = 5, y = 10;
 	cout << "Values before swapping: x = " << x << " , y = " << y << endl;
-	
+
 	swapByAddress(&x, &y);
-	cout<<"Valutes after swapping by address:  x = " << x << " , y = " << y << endl;
+	cout << "Valutes after swapping by address:  x = " << x << " , y = " << y << endl;
 
 	swapByRefrence(x, y);
 	cout << "Valutes after swapping by reference:  x = " << x << " , y = " << y << endl;
